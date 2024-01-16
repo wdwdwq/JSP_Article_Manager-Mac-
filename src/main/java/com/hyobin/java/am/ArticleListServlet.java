bf14bbdc90f4192f3c6c5e98fc4b426ca320f47e
wdwdwq <qkenr7278@gmail.com>
2024-01-16 23:48:01 +0900

    DB연결. servlet에서 직접 응답으로 테스트

---
+++ src/main/java/com/hyobin/java/am/ArticleListServlet.java
@@ -0,0 +1,55 @@
+package com.hyobin.java.am;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import com.hyobin.java.am.util.DBUtil;
+import com.hyobin.java.am.util.SecSql;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/article/list")
+public class ArticleListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html; charset=UTF-8;");
+		
+		Connection conn = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String url = "jdbc:mysql://127.0.0.1:3306/JAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+			conn = DriverManager.getConnection(url, "root", "");
+			
+			SecSql sql = SecSql.from("SELECT * FROM article");
+			
+			List<Map<String, Object>> articleListMap = DBUtil.selectRows(conn, sql);
+		
+			response.getWriter().append("<div>" + articleListMap.toString() + "</div>");
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패");
+		} catch (SQLException e) {
+			System.out.println("에러: " + e);
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+}
--- src/main/java/com/hyobin/java/am/HomeMainServlet.java
+++ src/main/java/com/hyobin/java/am/HomeMainServlet.java
@@ -9,13 +9,37 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/home/main")
+@WebServlet("/home/printDan")
 public class HomeMainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		response.getWriter().append("Hello");
+		
+		response.setContentType("text/html; charset=UTF-8;");
+		
+		String inputDan = request.getParameter("dan");
+		String inputLimit = request.getParameter("limit");
+		String inputColor = request.getParameter("color");
+		
+		if (inputDan == null || inputDan.equals("")) {
+			inputDan = "1";
+		}
+		if (inputLimit == null || inputLimit.equals("")) {
+			inputLimit = "1";
+		}
+		if (inputColor == null || inputColor.equals("")) {
+			inputColor = "black";
+		}
+		
+		int dan = Integer.parseInt(inputDan);
+		int limit = Integer.parseInt(inputLimit);
+		
+		response.getWriter().append(String.format("<div style=\"color:%s;\">== %d단 ==</div>", inputColor, dan));
+		
+		for (int i = 1; i <= limit; i++) {
+			response.getWriter().append(String.format("<div style=\"color:%s;\">%d * %d = %d</div>", inputColor, dan, i, dan * i));
+		}
+		
 	}
 
 }
